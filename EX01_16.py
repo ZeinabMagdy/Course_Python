656e0cd7daf8e89d7ecaeb10ece4a3c414278c61
ZeinabMagdy <zeinabmagdy63@gmail.com>
2022-10-20 22:04:48 +0200

    EX01_16

---
+++ EX01_16.py
@@ -0,0 +1,26 @@
+import turtle
+
+turtle.color("black")
+turtle.penup()
+turtle.goto(50, 60)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.color("black")
+turtle.penup()
+turtle.goto(-50, 60)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.color("black")
+turtle.penup()
+turtle.goto(-50, -40)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.color("black")
+turtle.penup()
+turtle.goto(50, -40)
+turtle.pendown()
+turtle.circle(50)
+turtle.done()
