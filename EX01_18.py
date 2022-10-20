89960c78c7d3a33947eff32e9b4372276552fb6a
ZeinabMagdy <zeinabmagdy63@gmail.com>
2022-10-20 22:09:25 +0200

    EX01_18

---
+++ EX01_18.py
@@ -0,0 +1,20 @@
+import turtle
+turtle.showturtle()
+
+turtle.color("blue")
+
+turtle.right(80)
+turtle.forward(160)
+
+turtle.right(145)
+turtle.forward(145)
+
+turtle.right(138)
+turtle.forward(150)
+
+turtle.right(135)
+turtle.forward(140)
+
+turtle.right(143)
+turtle.forward(157)
+
