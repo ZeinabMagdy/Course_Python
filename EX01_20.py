d939bbc748e8531c93e726b63c48642a16f73436
ZeinabMagdy <zeinabmagdy63@gmail.com>
2022-10-20 22:12:06 +0200

    EX01_20

---
+++ EX01_20.py
@@ -0,0 +1,53 @@
+import turtle
+
+turtle.penup()
+turtle.goto(100, 100)
+turtle.pendown()
+turtle.goto(-100, 100)
+turtle.goto(-100, -50)
+turtle.goto(100, -50)
+turtle.goto(100, 100)
+
+turtle.penup()
+turtle.goto(0, 0)
+turtle.pendown()
+turtle.goto(-200, 0)
+turtle.goto(-200, -140)
+turtle.goto(0, -140)
+turtle.goto(0, 0)
+
+turtle.penup()
+turtle.goto(100, 100)
+turtle.pendown()
+turtle.goto(0, 0)
+
+turtle.penup()
+turtle.goto(-100, 100)
+turtle.pendown()
+turtle.goto(-200, 0)
+
+turtle.penup()
+turtle.goto(-100, -50)
+turtle.pendown()
+turtle.goto(-200, -140)
+
+turtle.penup()
+turtle.goto(100, -50)
+turtle.pendown()
+turtle.goto(0, -140)
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
