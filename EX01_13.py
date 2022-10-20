d9adae7c11af3216da22a395ca4dc79c3aaad8b3
ZeinabMagdy <zeinabmagdy63@gmail.com>
2022-10-20 22:00:50 +0200

    EX01_13

---
+++ EX01_13.py
@@ -0,0 +1,16 @@
+import turtle
+
+
+#plus
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(180)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(180)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.done()
