81f3a0b3bd7645ef8af711cb4a388b3ecc123570
ZeinabMagdy <zeinabmagdy63@gmail.com>
2022-10-20 22:02:15 +0200

    EX01_14

---
+++ EX01_14.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.showturtle()
+
+#Triangle
+turtle.right(50)
+turtle.forward(150)
+turtle.right(130)
+turtle.forward(150)
+turtle.right(115)
+turtle.forward(130)
+turtle.done()
