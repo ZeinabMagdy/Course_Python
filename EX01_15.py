2ec5442f9af81379db6cd950646fa30ea3d9c617
ZeinabMagdy <zeinabmagdy63@gmail.com>
2022-10-20 22:03:31 +0200

    EX01_15

---
+++ EX01_15.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.showturtle()
+
+
+#Two Triangle
+
+turtle.forward(150)
+turtle.right(130)
+turtle.forward(150)
+turtle.right(115)
+turtle.forward(260)
+turtle.left(115)
+turtle.forward(150)
+turtle.left(128)
+turtle.forward(155)
+turtle.done()
