4d3efc1ba3f61dfd05662d65b19893cdf351a74c
ZeinabMagdy <zeinabmagdy63@gmail.com>
2022-10-20 14:24:26 +0200

    EX01_03

---
+++ EX01_03.py
@@ -0,0 +1,1 @@
+print("FFFFFFF    U        U    NN      NN\nFF         U        U    NNN     NN\nFFFFFFF    U        U    NN N    NN\nFF          U      U     NN N    NN\nFF            UUU        NN      NNN")
