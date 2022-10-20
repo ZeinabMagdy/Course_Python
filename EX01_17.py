2a44d38cb9a2fe7e2ea1f1f1042d801f26af5459
ZeinabMagdy <zeinabmagdy63@gmail.com>
2022-10-20 22:07:24 +0200

    EX01_17

---
+++ EX01_17.py
@@ -0,0 +1,9 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-39, 48)
+turtle.write("(-39, 48)", font = ("Arial", 12, 'bold'))
+turtle.pendown()
+turtle.goto(50, -50)
+turtle.write("(50, -50)", font = ("Arial", 12, 'bold'))
+turtle.done()
