91d1f7025309afb08a785b143833cccfd25c2db7
ZeinabMagdy <110781912+ZeinabMagdy@users.noreply.github.com>
2022-10-24 19:28:22 +0200

    updated EX01_17

--- EX01_17.py
+++ EX01_17.py
@@ -2,8 +2,8 @@ import turtle
 
 turtle.penup()
 turtle.goto(-39, 48)
-turtle.write("(-39, 48)", font = ("Arial", 12, 'bold'))
+turtle.write("(-39, 48)")
 turtle.pendown()
 turtle.goto(50, -50)
-turtle.write("(50, -50)", font = ("Arial", 12, 'bold'))
+turtle.write("(50, -50)")
 turtle.done()
